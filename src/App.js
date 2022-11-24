ca52afc55c4f49cf8f32bb50cfb8f7598e4778fc
Rami _ <ramiroberardi7@gmail.com>
2022-11-23 23:55:27 -0300

    HOLA

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+
 
 function App() {
   return (
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,32 @@
+import './Navbar.css'
+import CartWidget from '../CartWidget/CartWidget'
+
+const Navbar = (props) => {
+    console.log(props)
+
+    const handleOnClick = () => {
+        alert('hice click')
+    }
+    
+    const handleOnClick2 = () => {
+        alert('hice otro click')
+    }
+
+
+    return (
+        <nav className='navbar'>
+            <h1 style={{ color: props.colorTitle}}>MyApp</h1>
+            <Button colorText='red' func={handleOnClick}>Home</Button>
+            <Button colorText='blue' func={handleOnClick2}>About</Button>
+            <Button>
+                Contact
+            </Button>
+            <Button>
+                Login
+            </Button>
+            <CartWidget />
+        </nav>
+    )
+}
+
+export default Navbar
